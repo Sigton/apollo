345adeaf885ee9a0a9988eaf09b89031dc469b85
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-12-01 09:26:19 +0000

    Updated constants.py

--- src/constants.py
+++ src/constants.py
@@ -17,3 +17,12 @@ PLAYER_SPEED = 2
 PLAYER_GRAVITY = 1
 PLAYER_FRICTION = 0.7
 PLAYER_JUMP_HEIGHT = 10
+
+# terrain engine constants
+
+# 0 - solid
+# 1 - obstacle
+TILE_TYPES = {
+    0: 0,
+    1: 1
+}
