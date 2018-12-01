6b54d76856975a8c57cea78a109f711ebd0c8d4f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-12-01 08:51:35 +0000

    Added a temp tile

--- src/tile.py
+++ src/tile.py
@@ -7,10 +7,10 @@ import pygame
 from src import spritesheet
 
 # list each platform with co-ords on spritesheet here
-
+GROUND = (0, 0, 32, 32)
 
 # tuple of all platforms
-platforms = ()
+platforms = (GROUND,)
 
 
 class Platform(pygame.sprite.Sprite):
@@ -19,4 +19,18 @@ class Platform(pygame.sprite.Sprite):
 
         pygame.sprite.Sprite.__init__(self)
 
-        sprite_sheet = sprite
+        sprite_sheet = spritesheet.SpriteSheet("src/resources/terrain.png")
+
+        self.image = sprite_sheet.get_image(spritesheetdata[0],
+                                            spritesheetdata[1],
+                                            spritesheetdata[2],
+                                            spritesheetdata[3])
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.layer = layer
+
+    def draw(self, display):
+        display.blit(self.image, (self.rect.x, self.rect.y))
