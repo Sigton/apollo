bcd4d91e8b2b159f039e62b94a878740d6f17c9e
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-12-01 09:05:47 +0000

    Added events loop

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 from src import constants
 from src import player
@@ -40,12 +41,32 @@ class Main:
 
     def run(self):
 
+        run = 0
+        jump = False
+
         # game loop
         while not self.game_exit:
             for event in pygame.event.get():
 
-                if event.type == pygame.QUIT:
+                if event.type == QUIT:
                     self.game_exit = True
+
+                elif event.type == KEYDOWN:
+                    if event.key in (K_LEFT, K_a):
+                        run = -1
+                    elif event.key in (K_RIGHT, K_d):
+                        run = 1
+
+                    if event.key in (K_SPACE, K_w, K_UP):
+                        jump = True
+
+                elif event.type == KEYUP:
+                    if event.key in (K_LEFT, K_a) and not run == 1:
+                        run = 0
+                    elif event.key in (K_RIGHT, K_d) and not run == -1:
+                        run = 0
+                    elif event.key in (K_SPACE, K_w, K_UP):
+                        jump = False
 
 
 if __name__ == "__main__":
