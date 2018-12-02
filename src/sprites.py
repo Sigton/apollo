288a0462340db18a5a2a199cb60a1bbcb1c6ae31
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-12-02 09:45:35 +0000

    Adding in bullets for aliens to shoot

--- src/sprites.py
+++ src/sprites.py
@@ -235,3 +235,19 @@ class Background(pygame.sprite.Sprite):
 
     def draw(self, display):
         display.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Bullet(ObstacleBase):
+
+    def __init__(self, x, y, speed=15):
+
+        ObstacleBase.__init__(self)
+
+        self.image = pygame.image.load("src/resources/bullet.png")
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.speed = speed
+        self.damage_factor = speed
